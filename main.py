54aaa6549770d41eb4db6fc51b41553b3e01abdd
shk0625 <tjfgml1238@gmail.com>
2023-10-25 22:58:35 +0900

    ⚡️ 순위표 내에 있는 사람들 몇등인지까지 반환

--- main.py
+++ main.py
@@ -171,16 +171,16 @@ async def ranking(ctx, member: discord.Member = None):
     cur.execute(sql, (str(member.id), today))
     rs = cur.fetchone()
 
-    if rs is not None:
+    if rs is None:
+        await ctx.send(f"**{member.display_name}**님, 출석체크부터 할까요?")
+    else:
         index = next((i for i, v in enumerate(result) if v['did'] == str(member.id)), None)
         if index is not None:
             if index < 5:
-                await ctx.send(f"**{member.display_name}**님은 순위표 내에 있어요!")
+                await ctx.send(f"**{member.display_name}**님은 순위표 내에 있어요! {index + 1}등 이에요!")
             else:
                 await ctx.send(
                     f"**{member.display_name}**님은 순위표에 보이지 않아요! 어디있죠? (찾는 중...)\n 엇 **{member.display_name}**님의 순위는 **{index + 1}**등입니다. 좀 더 분발하세요!!")
-    else:
-        await ctx.send(f"**{member.display_name}**님, 출석체크부터 할까요?")
 
 
 @bot.command(aliases=['도움말', 'hp'])
